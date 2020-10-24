301fff262f1fdf32a57146de9e48b7f71e8ae70e
Phan Văn Minh <58026771+Phanminh118@users.noreply.github.com>
2020-10-25 00:52:11 +0700

    nhap lieu

---
+++ lib/app/home/library/nhap-lieu/muon-tra/borrow.view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class BorrowLibrary extends StatefulWidget {
+  @override
+  _BorrowLibraryState createState() => _BorrowLibraryState();
+}
+
+class _BorrowLibraryState extends State<BorrowLibrary> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
